39a80290fbb44f4e664b2d9c3aba7cdfe80f75b6
Vladimir Glushkov <xhtmlblog@gmail.com>
2012-05-29 15:23:40 +0600

    Главная и всплывашки

--- app/templates/pricestat.js
+++ app/templates/pricestat.js
@@ -4,7 +4,7 @@ templates.pricestat = {
     },
     stat: {
         tpl: 
-             '<div class="category-sub-name">{subname}</div><div class="info-icon">i</div>'+
+             '<div class="category-sub-name">{subname}</div><div class="info-icon"></div>'+
              '<ul class="list">'+
              '<tpl for="categoryitems">'+
                 '<li><span class="name">{name}</span> <span class="price">{price} <b class="rub">a</b><b class="price-change-{pricechange}"></b></span></li>'+
