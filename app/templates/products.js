da74d0259f9245f329fcb724d078a6e0ea3fcc71
Segrey Belozerov <belozerow@gmail.com>
2012-06-01 11:46:21 +0600

    [add] Добавил классы корзинам в зависимости от типа корзины

--- app/templates/products.js
+++ app/templates/products.js
@@ -31,13 +31,22 @@ templates.products = {
     cart: {
         //name,custom,info,image, minprice, maxprice
         itemTpl:
-                '<div class="prodcarts">'+
-                    '<a href="#/products/carts/{id}"><img src="{image}" style="height: 150px; width: 162px;"/></a>'+
-                    '<div class="info-icon cart-info"></div>'+
-                    '<div class="add">Добавить в список</div>'+
-                '</div>'+
-                '<div class="cart-name">{name}</div>'+
-                '<div class="cart-price">{minprice} - {maxprice}<b class="rub">a</b></div>'
+                new Ext.XTemplate('<div class="cart-style cart-style-{[this.getCartId(values)]}">'+
+                    '<div class="prodcarts">'+
+                        '<a href="#/products/carts/{id}"><img src="{image}" style="height: 150px; width: 162px;"/></a>'+
+                        '<div class="info-icon cart-info"></div>'+
+                        '<div class="add">Добавить в список</div>'+
+                    '</div>'+
+                    '<div class="cart-name">{name}</div>'+
+                    '<div class="cart-price">{minprice} - {maxprice}<b class="rub">a</b></div>'+
+                '</div>',{
+                    cartNum: 0,
+                    getCartId: function(values){
+                        if(values.custom)
+                            return 'custom';
+                        return this.cartNum++;
+                    }
+                })
     },
     sliderinfo: {
         tpl: '<div class="slider-title">{name}</div>'+
