71d9317c54db2e5c5959370b3f60180b235fe3d1
Segrey Belozerov <belozerow@gmail.com>
2012-06-07 12:27:53 +0600

    [cng] send modal panel

--- app/view/SendForm.js
+++ app/view/SendForm.js
@@ -1,40 +1,62 @@
 Ext.define('Vzabote.view.SendForm',{
     extend: 'Vzabote.view.ModalPanel',
     alias: 'widget.sendform',
-    formTitle: '',
-    fieldEmptyText: '',
-    formValidation: '',
     sendType: 'sms', //sms/email/book
+    layout: {
+        type: 'vbox',
+        align: 'stretch',
+        pack: 'center'
+    },
     initComponent: function(){
         this.callParent();
-        this.add([{
-            xtype: 'container',
-            html: this.formTitle
-        },Ext.apply({
-            id: 'sendform-input',
-            emptyText: this.fieldEmptyText,
-            listeners: {
-                specialkey: function(field,e){
-                    if(e.getKey() == e.ENTER)
-                        this.submit();
-                },
-                scope: this
-            }
-        },this.formValidation),{
-            xtype: 'button',
-            text: 'Отправить',
-            handler: function(){
-                this.successPanel({code:100});
-                this.submit();
-            },
-            scope: this
-        }]);
-    },
+        var sendType = this.sendType;
+        this.sendform = this.add({
+            xtype: 'form',
+            defaultType: 'textfield',
+            items:[
+                Ext.apply({
+                    xtype: 'container'
+                },templates.modal['title'+sendType]),Ext.apply({
+                    id: 'sendform-input',
+                    allowBlank: false,
+                    listeners: {
+                        specialkey: function(field,e){
+                            if(e.getKey() == e.ENTER)
+                                this.submit();
+                        },
+                        scope: this
+                    }
+                },this['getParamsFor'+sendType]),{
+                    xtype: 'button',
+                    text: 'Отправить',
+                    handler: function(){
+                        this.successPanel({code: 195837});
+                        this.sendform.submit();
+                    },
+                    scope: this
+                }
+            ] 
+        });
+     },
     successPanel: function(data){
         this.removeAll();
         this.add(Ext.apply({
             xtype: 'container',
             data: data
         },templates.modal['success'+this.sendType]));
+    },
+    getParamsForsms: function(){
+        return {
+            emptyText: 'Введите номер мобильного телефона'
+        };
+    },
+    getParamsForbook: function(){
+        return this.getParamsForsms();
+    },
+    getParamsForemail: function(){
+        return {
+            emptyText: 'Введите адрес почты',
+            vtype: 'email'
+        };
     }
 });
